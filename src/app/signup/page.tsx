1408f72c9f097b600db3f5611f3183be4e512534
harshitha <harshitham1203@gmail.com>
2026-01-07 21:14:36 -0600

    checking sign up page link thing works

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUpPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-background">
+      <h1 className="text-2xl text-[#634E34] font-sans">Sign Up Page</h1>
+    </main>
+  );
+}
