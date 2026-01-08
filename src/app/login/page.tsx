de0a052981862bb8c6c9aabb340b67a5e06d1147
harshitha <harshitham1203@gmail.com>
2026-01-07 20:06:46 -0600

    added public sans font and set up login and signup pages

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-background">
+      <div className="flex h-[500px] w-[500px] items-center justify-center border border-gray-200">
+        <p className="font-sans text-xl text-white">login</p>
+      </div>
+    </main>
+  );
+}
