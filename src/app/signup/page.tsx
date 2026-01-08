dc7b93ba41940960a3517a330c238d7cd17bc1d0
harshitha <harshitham1203@gmail.com>
2026-01-07 21:16:25 -0600

    added note background thing to signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+
 export default function SignUpPage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-background">
-      <h1 className="text-2xl text-[#634E34] font-sans">Sign Up Page</h1>
+      <div className="relative flex h-[600px] w-[600px] items-center justify-center p-12">
+        <div className="absolute inset-0">
+          <Image
+            src="/assets/title-note.png"
+            alt="note background"
+            fill
+            className="object-contain"
+            priority
+          />
+        </div>
+      </div>
     </main>
   );
 }
