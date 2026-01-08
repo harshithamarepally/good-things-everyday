f1ee7b0a449b88ccca32a6a1b0bbd361289d1575
harshitha <harshitham1203@gmail.com>
2026-01-07 20:37:06 -0600

    added username and password fields for login page form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+
 export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-background">
-      <div className="flex h-[500px] w-[500px] items-center justify-center border border-gray-200">
-        <p className="font-sans text-xl text-white">login</p>
+      <div className="relative flex h-[600px] w-[600px] items-center justify-center p-12">
+        <div className="absolute inset-0">
+          <Image
+            src="/assets/title-note.png"
+            alt="note background"
+            fill
+            className="object-contain"
+            priority
+          />
+        </div>
+
+        <div className="relative z-10 flex w-full flex-col items-center gap-4 pt-10 text-center">
+          <form className="flex w-3/4 flex-col gap-4 mt-10">
+            <input
+              type="text"
+              placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="border-b-2 border-[#634E34] bg-transparent py-2 text-center text-lg text-[#634E34] placeholder:text-[#967751] font-sans focus:outline-none"
+            />
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border-b-2 border-[#634E34] bg-transparent py-2 text-center text-lg text-[#634E34] placeholder:text-[#967751] font-sans focus:outline-none"
+            />
+
+            <button className="mt-4 rounded-full bg-[#634E34] px-6 py-2 text-[#F5E9D3] hover:scale-105 transition-transform font-sans">
+              Sign In
+            </button>
+          </form>
+        </div>
       </div>
     </main>
   );
